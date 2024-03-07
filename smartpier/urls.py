b7cc99b504de1f2ecbff2a6fe40f29a3dc199430
Marco <58324229+m16mp5@users.noreply.github.com>
2024-03-07 12:07:00 +0800

    .

--- smartpier/urls.py
+++ smartpier/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from webapp import views                                           # import modules in views.py 
-
 
 
 urlpatterns = [
